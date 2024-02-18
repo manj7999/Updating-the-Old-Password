c76b63f13d8f0d3c6e02de4eafe25644b0835652
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-02-18 16:00:31 +0530

    Implement the Products Page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { Link } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Product";
-import MainHeader from './components/MainHeader';
+import ProductDetails from "./pages/ProductDetails";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <header>
-      
-      </header>
+      <MainHeader />
       <main>
-      <Link to="/welcome">
-        <Welcome />
-      </Link>
-      <Link to="/products">
-        <Products />
-      </Link>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='welcome'/>
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
